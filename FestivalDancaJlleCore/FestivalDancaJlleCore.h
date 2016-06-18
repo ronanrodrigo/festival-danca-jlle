c36ca236381d03119260b07db4e36f54abbfed1a
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-06-18 02:57:09 -0300

    Added core module

---
+++ FestivalDancaJlleCore/FestivalDancaJlleCore.h
@@ -0,0 +1,19 @@
+//
+//  FestivalDancaJlleCore.h
+//  FestivalDancaJlleCore
+//
+//  Created by Ronan Rodrigo Nunes on 18/06/16.
+//  Copyright © 2016 Ronan Rodrigo Nunes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FestivalDancaJlleCore.
+FOUNDATION_EXPORT double FestivalDancaJlleCoreVersionNumber;
+
+//! Project version string for FestivalDancaJlleCore.
+FOUNDATION_EXPORT const unsigned char FestivalDancaJlleCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FestivalDancaJlleCore/PublicHeader.h>
+
+
